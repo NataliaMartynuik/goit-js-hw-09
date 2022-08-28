a03cb0c01e14785f4bf766741855c5bac087dd6a
NataliaMartynuik <98057592+NataliaMartynuik@users.noreply.github.com>
2022-08-28 09:20:09 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const PROMPT_DELAY = 1000;
 
+const startBtn = document.querySelector('[data-start]');
+const bodyRef = document.querySelector('body');
+const stopBtn = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyColor() {
+  intervalId = setInterval(() => {
+    let colorValue = getRandomHexColor();
+    bodyRef.style.backgroundColor = colorValue;
+    startBtn.setAttribute('disabled', 'disabled');
+  }, PROMPT_DELAY);
+};
+
+function stopChangeBodyColor() {
+  clearInterval(intervalId);
+  startBtn.removeAttribute('disabled', 'disabled');
+};
+
+startBtn.addEventListener('click', changeBodyColor);
+stopBtn.addEventListener('click', stopChangeBodyColor);
